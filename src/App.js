88075ea0908e0d9dde4bbafcfa37ce3f90c9499f
Sushant <sdangol1999@gmail.com>
2022-11-16 14:47:18 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+const getRandomHexColor = () => {
+  const randomHex = [
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
+  ];
+
+  const color =  new Array(6).fill('')
+  .map(() => randomHex[Math.floor(Math.random() * randomHex.length)])
+  .join('');
+
+  return `#${color}`;
+}
 
 function App() {
+
+  const [color, setColor] = useState();
+  const [answers, setAnswers] = useState([]);
+  const [isWrong, setIsWrong] = useState(false);
+
+  function pickColor() {
+    getRandomHexColor();
+
+    const randomColor = getRandomHexColor();
+
+    setColor(randomColor);
+
+    setAnswers([randomColor, getRandomHexColor(), getRandomHexColor()].sort(
+      () => 0.5 - Math.random()
+    ));
+  }
+
+  useEffect(() => {
+    pickColor();
+  }, []);
+
+  function checkAnswer(answer) {
+    if(answer === color) {
+      setIsWrong(false);
+      pickColor();
+    } else {
+      setIsWrong(true);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div className="color-guess"
+            style={{ background: color }}
+        ></div>   
+
+        {answers.map((answer) => (
+          <button key={answer}
+          onClick={() => checkAnswer(answer)}>
+            {answer}
+          </button>
+        ))}
+
+        {isWrong && <div className="wrong">Wrong Answer</div>}
+      </div>
     </div>
   );
 }
